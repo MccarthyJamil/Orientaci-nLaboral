0ffeee51d1aed673fd5afb296a4e32c6553176fd
Jamil <esp.jamilmccarthy@gmail.com>
2025-09-29 09:36:42 -0600

    Host en línea

--- js/script.js
+++ js/script.js
@@ -1,36 +1,4 @@
-// CÓDIGO PARA LA PÁGINA DE EXPERIENCIAS (GALERÍA Y RESEÑAS)
-// Este código solo se ejecutará si el elemento con el ID 'gallery-container' existe
-if (document.getElementById('gallery-container')) {
-    const lectores = [
-        { nombre: 'Adrián A', flor: 'ADRIAN_A.png' },
-        { nombre: 'Adrián M', flor: 'ADRIAN_M.png' },
-        { nombre: 'Libi C', flor: 'LIBI_C.png' },
-        { nombre: 'Jenni G', flor: 'JENNI_G.png' }
-    ];
 
-    const galleryContainer = document.getElementById('gallery-container');
-
-    function crearElementoGaleria(lector) {
-        const galleryItem = document.createElement('div');
-        galleryItem.className = 'gallery-item';
-
-        const flowerImage = document.createElement('img');
-        flowerImage.src = `images/${lector.flor}`;
-        flowerImage.alt = `Flor para ${lector.nombre}`;
-
-        const lectorName = document.createElement('p');
-        lectorName.textContent = lector.nombre;
-
-        galleryItem.appendChild(flowerImage);
-        galleryItem.appendChild(lectorName);
-
-        galleryContainer.appendChild(galleryItem);
-    }
-
-    lectores.forEach(lector => {
-        crearElementoGaleria(lector);
-    });
-}
 // Espera a que el contenido de la pagina se cargue completamente
 document.addEventListener('DOMContentLoaded', () => {
 
